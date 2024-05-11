53dccfaae6e6806dd556d6a23022dc013c37645f
IlyesBB <ilyes.boussama@yahoo.fr>
2024-05-11 23:01:18 +0200

    Removing wrong imports

--- plot/__init__.py
+++ plot/__init__.py
@@ -1,5 +1,5 @@
 from .coefplot import coefplot
 from .contingencyplot import contingencyplot
-from .pairplot_quanti import pairplot_quanti, pearson_coefficient
-from .pairplot_quali import pairplot_quali, contingence_coefficient
+from .pairplot_quanti import (pairplot_quanti, pearson_coefficient, lower_plot as lower_plot_quanti)
+from .pairplot_quali import (pairplot_quali, contingence_coefficient, lower_plot as lower_plot_quali)
 from .parallelplot import parallelplot
