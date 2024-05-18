f99adaae2ab1c1f129f9b0ca60f55aac6b64b658
IlyesBB <ilyes.boussama@yahoo.fr>
2024-05-18 20:48:18 +0200

    Adding import

--- plot/__init__.py
+++ plot/__init__.py
@@ -3,3 +3,4 @@ from .contingencyplot import contingencyplot
 from .pairplot_quanti import (pairplot_quanti, pearson_coefficient, lower_plot as lower_plot_quanti)
 from .pairplot_quali import (pairplot_quali, contingence_coefficient, lower_plot as lower_plot_quali)
 from .parallelplot import parallelplot
+from .pairplot import pairplot
